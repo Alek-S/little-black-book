69644e0cc1c2a63e10dcdb822219b93ddaa1a9a0
Alek Shnayder <alek.shnayder@gmail.com>
2017-05-23 20:08:47 -0500

    Controller: API - Add new mate entry

--- controllers/api_routes.js
+++ controllers/api_routes.js
@@ -8,12 +8,44 @@ module.exports = function(app) {
 
 	//get user by ID
 	app.get('/api/user/:id', (req, res)=>{
-		db.User.findOne({
-			where: { id: req.params.id}
+		
+		if(req.params.id !== 'all'){
+			db.User.findOne({
+				where: { id: req.params.id}
+			}).then( (result)=>{
+				res.json(result);
+			});
+		}else{
+			db.User.findAll({
+			}).then( (result)=>{
+				res.json(result);
+			});
+		}
+	});
+
+	//create new mate
+	app.post('/api/mate/:name/:platform/:userID', (req, res)=>{
+		db.Mate.create({
+			name: req.params.name,
+			platform: req.params.platform,
+			UserId: req.params.userID,
 		}).then( (result)=>{
 			res.json(result);
 		});
 	});
 
+	//create new event
+	// app.post('/api/event/:what/:when/:where/:rating/:UserID/:MateID', (req, res)=>{
+	// 	db.Mate.create({
+	// 		what: req.params.what,
+	// 		when: req.params.when,
+	// 		where: req.params.where,
+	// 		rating: req.params.rating,
+	// 		UserId: req.params.userID,
+	// 		MateId: req.params.MateID,
+	// 	}).then( (result)=>{
+	// 		res.json(result);
+	// 	});
+	// });
 
 };
