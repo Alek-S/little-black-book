b91cd0eb0c2ed08252b9a679cd2a5de2913363c0
Alek Shnayder <alek.shnayder@gmail.com>
2017-05-30 20:00:12 -0500

    Recoded the User page and CSS from scratch

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,7 @@
 $( document ).ready(function(){
-    $('.toggle').click(function(e) {
-        e.preventDefault();
 
-        var $this = $(this);
-
-        if ($this.next().hasClass('show')) {
-            $this.next().removeClass('show');
-            $this.next().slideUp(350);
-        } else {
-            $this.parent().parent().find('li .inner').removeClass('show');
-            $this.parent().parent().find('li .inner').slideUp(350);
-            $this.next().toggleClass('show');
-            $this.next().slideToggle(350);
-        }
+    $('.mate .mHead').on('click', function(){
+        event.preventDefault();
+        $(this).siblings('.events').slideToggle();
     });
 });
